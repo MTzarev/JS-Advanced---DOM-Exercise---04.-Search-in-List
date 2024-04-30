eb9270c0bf916e9948ebf2e82bbf19457f0c644b
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-30 20:48:53 +0300

    Update search.js

--- search.js
+++ search.js
@@ -4,18 +4,15 @@ function search() {
    let input = document.querySelector(`input`).value;
    let result = document.getElementById(`result`);
    let sum = 0;
-
    for (let letters of list) {
       if ((letters.textContent).toLowerCase().includes(input.toLowerCase())) {
          letters.style.fontWeight = `bold`;
-         letters.style.textDecoration = `underline`
+         letters.style.textDecoration = `underline`;
          sum++
       } else {
          letters.style.fontWeight = ``;
          letters.style.tectDecoration = ``;
       }
    }
-
-   result.textContent = `${sum} matches found`
-
+   result.textContent = `${sum} matches found`;
 }
